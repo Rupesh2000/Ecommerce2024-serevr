76a0f6bce1a15ad7a1cc5072012b7ec60d9a2237
rupesh <rupesh25112000@gmail.com>
2024-01-16 12:50:29 +0530

    Model and Hashing, Register User

---
+++ controllers/authController.js
@@ -0,0 +1,59 @@
+import { hashPassword } from "../helpers/authHelper.js";
+import userModel from "../models/userModel.js";
+
+export const registerController = async (req, res) => {
+  try {
+    const { name, email, password, phone, address } = req.body;
+
+    //validation
+    if (!name) {
+      return res.send({ error: "Name is Required" });
+    }
+    if (!email) {
+      return res.send({ error: "Email is Required" });
+    }
+    if (!password) {
+      return res.send({ error: "Password is Required" });
+    }
+    if (!phone) {
+      return res.send({ error: "Phone is Required" });
+    }
+    if (!address) {
+      return res.send({ error: "address is Required" });
+    }
+    //check user
+    const existingUser = await userModel.findOne({ email });
+
+    //existing user
+    if (existingUser) {
+      return res.status(200).send({
+        success: true,
+        message: "Already Register please login",
+      });
+    }
+    //register user
+    const hashedPassword = await hashPassword(password);
+
+    //save
+    const user = await new userModel({
+      name,
+      email,
+      phone,
+      address,
+      password: hashedPassword,
+    }).save();
+
+    res.status(201).send({
+      success: true,
+      message: "User Register successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in Registration",
+      error,
+    });
+  }
+};
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
 
 //configure env
 dotenv.config();
@@ -16,6 +17,9 @@ const app = express();
 //middlewares
 app.use(express.json());
 app.use(morgan("dev"));
+
+//routes
+app.use("/api/v1/auth", authRoutes);
 
 //rest api
 app.get("/", (req, res) => {
