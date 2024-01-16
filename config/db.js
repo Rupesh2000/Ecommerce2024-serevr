0ff817239f92c4810c175aabbf62ae57238f4322
rupesh <rupesh25112000@gmail.com>
2024-01-16 10:40:08 +0530

    Database Connect MongoDB

---
+++ config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import colors from "colors";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    console.log(
+      `Connected To Mongodb Database ${conn.connection.host}`.bgMagenta.white
+    );
+  } catch (error) {
+    console.log(`Error in Mongodb ${error}`.bgRed.white);
+  }
+};
+
+export default connectDB;
--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
 
 //configure env
 dotenv.config();
 
+//database config
+connectDB();
+
 //rest object
 const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(morgan("dev"));
 
 //rest api
 app.get("/", (req, res) => {
