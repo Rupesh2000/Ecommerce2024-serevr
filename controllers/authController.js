5160f9daed60b4126b5a0bcf8628e90aab41a77a
rupesh <rupesh25112000@gmail.com>
2024-01-18 18:53:27 +0530

    Added some library

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,19 +8,19 @@ export const registerController = async (req, res) => {
 
     //validation
     if (!name) {
-      return res.send({ error: "Name is Required" });
+      return res.send({ message: "Name is Required" });
     }
     if (!email) {
-      return res.send({ error: "Email is Required" });
+      return res.send({ message: "Email is Required" });
     }
     if (!password) {
-      return res.send({ error: "Password is Required" });
+      return res.send({ message: "Password is Required" });
     }
     if (!phone) {
-      return res.send({ error: "Phone is Required" });
+      return res.send({ message: "Phone is Required" });
     }
     if (!address) {
-      return res.send({ error: "address is Required" });
+      return res.send({ message: "address is Required" });
     }
     //check user
     const existingUser = await userModel.findOne({ email });
@@ -28,7 +28,7 @@ export const registerController = async (req, res) => {
     //existing user
     if (existingUser) {
       return res.status(200).send({
-        success: true,
+        success: false,
         message: "Already Register please login",
       });
     }
