5d1a5964156462e1eb12aed2544d8fbd4739b9fa
rupesh <rupesh25112000@gmail.com>
2024-01-19 20:06:51 +0530

    Added User-Auth

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -19,4 +19,9 @@ router.post("/login", loginController);
 //test routes
 router.get("/test", requireSignIn, isAdmin, testController);
 
+//protected route auth
+router.get("/user-auth", requireSignIn, (req, res) => {
+  res.status(200).send({ ok: true });
+});
+
 export default router;
