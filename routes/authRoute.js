be96dc7b6ea346a3955eda9ff5fff62371df6dce
rupesh <rupesh25112000@gmail.com>
2024-01-22 00:49:37 +0530

    Admin and User Panel

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -23,8 +23,13 @@ router.post("/forgot-password", forgotPasswordController);
 //test routes
 router.get("/test", requireSignIn, isAdmin, testController);
 
-//protected route auth
+//protected User route auth
 router.get("/user-auth", requireSignIn, (req, res) => {
+  res.status(200).send({ ok: true });
+});
+
+//protected Admin route auth
+router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
   res.status(200).send({ ok: true });
 });
 
